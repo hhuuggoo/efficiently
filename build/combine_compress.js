c5ffe13ca886d243166e985981863618a3707a2c
Hugo <hugoshi@blob.(none)>
2012-01-02 20:17:23 -0500

    starting build process

---
+++ build/combine_compress.js
@@ -0,0 +1,13 @@
+[{"name" : "basejs",
+  "compressor" : "java -jar build/yuicompressor-2.4.7.jar",
+  "inputs" : ["static/js/underscore.js",
+	      "static/js/jquery-1.6.4.min.js",
+	      "static/js/jquery.autoresize.js",
+	      "static/js/jquery-ui-1.8.16.custom.js",
+	      "static/js/tabs.js",
+	      "static/js/underscore.string.js"],
+  "output" : "static/js/base.min.js",
+  "output_link" : "/static/js/base.min.js",
+  "html_dirs" : ["templates"],
+  "type" : "js"}]
+
