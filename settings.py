cb51dabccb940026288eecbefbd0d0c5dac1788a
Your Name <you@example.com>
2012-12-28 16:33:16 -0500

    prod port

--- settings.py
+++ settings.py
@@ -4,12 +4,12 @@ settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
     "cookie_secret": "sdfasdfasdf;klj;lkj;sdfsll=sd",
     "xsrf_cookies": False,
-    'debug' : True,
+    'debug' : False,
     'login_url' : '/login',
     'ssl_options' : {'certfile' : "/etc/nginx/server.crt",
                      'keyfile' : "/etc/nginx/server.key"},
     'enabled_protocols' : ['xhr-multipart', 'xhr-polling'],
-    'socket_io_port' : 9050
+    'socket_io_port' : 9000
     }
 
 
